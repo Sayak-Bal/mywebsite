b0b2fa52847b5be4fe157cce3eb2f20e1fcf160e
Sayak-Bal <sayakbal005@gmail.com>
2023-11-12 20:18:12 +0530

    Merge remote changes

--- abc.sh
+++
@@ -1,7 +0,0 @@
-echo "# resumesayak" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Sayak-Bal/mywebsite.git
-git push -u origin main
